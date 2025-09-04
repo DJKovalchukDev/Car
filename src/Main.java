aa788bcf5bc9fb797a38bcf87a3ca5436bf2e5df
DJKovalchukDEV <dmitrii.kovalchuk@gmx.de>
2025-09-04 19:09:56 +0200

    Added .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    Engine engine = new Engine(EngineType.GAS, 2500, 250);
-    Car car = new Car(engine, CarType.CABRIOLET,3500);
+        Engine engine = new Engine(EngineType.GAS, 2500, 250);
+        Car car = new Car(engine, CarType.CABRIOLET, 3500);
         System.out.println("Тип авто / CarType: " + car.getCarType());
         System.out.println("Тип двигателя / EngineType: " + car.getEngine().getEngineType());
         System.out.println("Объем / EngineVolume: " + car.getEngine().getVolume());
